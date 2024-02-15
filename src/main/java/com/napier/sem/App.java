cdf030dda64ccd85b2aa290d9a9b4be82f7634bd
vintagefuture <pinkpallyn@gmail.com>
2024-02-15 21:06:31 +0000

    Get all salaries by role

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,8 @@
 package com.napier.sem;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App
 {
@@ -137,6 +139,48 @@ public class App
         }
     }
 
+    public List<Employee> salariesByRoleReport(String role)
+    {
+        List<Employee> employees = new ArrayList<>();
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+
+            String strSelect = "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary " +
+                    "FROM employees, salaries, titles " +
+                    "WHERE employees.emp_no = salaries.emp_no " +
+                    "AND employees.emp_no = titles.emp_no " +
+                    "AND salaries.to_date = '9999-01-01' " +
+                    "AND titles.to_date = '9999-01-01' " +
+                    "AND titles.title = '" + role + "' " +
+                    "ORDER BY employees.emp_no ASC";
+
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returnedw
+            while (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("employees.emp_no");
+                emp.first_name = rset.getString("employees.first_name");
+                emp.last_name = rset.getString("employees.last_name");
+                emp.salary = rset.getInt("salaries.salary");
+                employees.add(emp);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+        return employees;
+    }
+
     public static void main(String[] args)
     {
         // Create new Application
@@ -147,6 +191,17 @@ public class App
 
         // Get Employee
         Employee emp = a.getEmployee(255530);
+
+        // Generate salaries report by role
+        List<Employee> employees = a.salariesByRoleReport("Engineer");
+        if (employees != null && !employees.isEmpty()) {
+            System.out.println("Salaries report by role Engineer");
+            for (Employee employee : employees) {
+                System.out.println(employee.first_name + employee.first_name + ": " + employee.salary);
+            }
+        } else {
+            System.out.println("No salaries found or failed to generate the report.");
+        }
 
         // Display results
         a.displayEmployee(emp);
